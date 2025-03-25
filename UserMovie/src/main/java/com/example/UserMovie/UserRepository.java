7d5b36a7cf472d6eca1f441e81784172d0a1ecbc
Ivana Lovric <ivana.lovric1314@gmail.com>
2025-03-25 21:29:12 +0100

    first functions with get and post mapping

---
+++ UserMovie/src/main/java/com/example/UserMovie/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.UserMovie;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
