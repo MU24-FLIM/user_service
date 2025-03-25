3ee273bf57d9eaa7ba4ddd53858b9ed4a80bbfdd
Ivana Lovric <ivana.lovric1314@gmail.com>
2025-03-25 12:20:50 +0100

    first commit

---
+++ UserMovie/src/main/java/com/example/UserMovie/UserMovieApplication.java
@@ -0,0 +1,13 @@
+package com.example.UserMovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMovieApplication.class, args);
+	}
+
+}
