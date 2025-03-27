a61847d4ff8aa9c31127d966288e155d3fdeeeb4
Ivana Lovric <ivana.lovric1314@gmail.com>
2025-03-27 16:23:34 +0100

    added data.sql - fix

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,9 @@
-INSERT IGNORE INTO movies (username, email, age, reviewId)
+INSERT IGNORE INTO users (username, email, age, review_id)
     VALUES
-       ('ivana', 'ivana@protonmail.com', 30, 2 )
-       ('madde', 'madde@gmail.se', 30, 1 )
-       ('linda', 'lin@blom.com', 38, 4 )
-       ('fredrik', 'freds@andr.se', 35, 3 )
-       ('strumpa', 'lila@strump.se', 55, 7 )
+       ('ivana', 'ivana@protonmail.com', 30, 2 ),
+       ('madde', 'madde@gmail.se', 30, 1 ),
+       ('linda', 'lin@blom.com', 38, 4 ),
+       ('fredrik', 'freds@andr.se', 35, 3 ),
+       ('strumpa', 'lila@strump.se', 55, 7 ),
        ('godis', 'god@livs.se', 23, 6 )
+;
